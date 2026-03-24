f6a92523d9daf8f536844c64fbc9646c170ef241
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-24 15:49:24 +0900

    sync: comments fot main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// ReactアプリをHTMLのどこに表示するかを指定
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
